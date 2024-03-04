b4f9adfb6aafd1f0510061ed72b0224b6c41befe
Vinakshirajput <157057790+Vinakshirajput@users.noreply.github.com>
2024-03-04 21:00:20 +0530

    Create TableOf2.java

---
+++ TableOf2.java
@@ -0,0 +1,13 @@
+class TableOf2 {
+    public static void main(String[] args) {
+        System.out.println("to print table of 2 ");
+        int n=0;
+        for(int i=1;i<=10;i++){
+            n=2*i;
+            System.out.println("2 * " + i + " = " + n);
+        }
+       
+       } 
+    
+    }
+    
