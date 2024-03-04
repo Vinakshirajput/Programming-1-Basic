452972221a9bae310afb77e1beced476db8c3ff0
Vinakshirajput <157057790+Vinakshirajput@users.noreply.github.com>
2024-03-04 20:15:28 +0530

    Create NaturalNumber.java

---
+++ NaturalNumber.java
@@ -0,0 +1,10 @@
+class NaturalNumber {
+    public static void main(String[] args) {
+        System.out.println("print a natural numbers ");
+        int i=0;
+        do{
+            i++;
+             System.out.println(i);
+        }while(i<=10);
+    }
+}
