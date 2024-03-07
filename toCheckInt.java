42e184c4d1223917b5d24a27f9548371a848bfd1
Vinakshirajput <157057790+Vinakshirajput@users.noreply.github.com>
2024-03-07 12:44:39 +0530

    Create toCheckInt.java

---
+++ toCheckInt.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class toCheckInt{
+public static void main(){
+System.out.println("enter the number");
+            Scanner x = new Scanner(System.in);
+
+            System.out.println(x.hasNextInt());
+}
+}
